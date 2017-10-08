66e19bfd0c73d366a4e008060c0df7252910ab1a
Che1 <nachwon@gmail.com>
2017-10-08 21:21:39 +0900

    Question, Choice model added

---
+++ mysite/polls/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f'설문조사: {self.title}'
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    title = models.CharField(max_length=50)
+    vote = models.IntegerField(default=0)
+
+    def __str(self):
+        return f'{self.title}: ({self.question.title})'
