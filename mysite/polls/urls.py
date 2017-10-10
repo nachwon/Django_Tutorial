ebba8fcca8c0d345882a495d0d1643c92fc759e2
Che1 <nachwon@gmail.com>
2017-10-10 09:54:45 +0900

    poll app developed

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,5 +4,8 @@ from polls import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'(?P<q_id>\d+)/$', views.detail, name='detail')
+    url(r'(?P<q_id>\d+)/$', views.detail, name='detail'),
+    url(r'(?P<q_id>\d+)/vote/$', views.vote, name='vote'),
+    url(r'(?P<q_id>\d+)/result/$', views.result, name='result'),
+    url(r'(?P<q_id>\d+)/result/revote/$', views.revote, name='revote')
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.datastructures import MultiValueDictKeyError
 
 from .models import *
 
@@ -20,3 +21,49 @@ def detail(request, q_id):
         'choice': choice,
     }
     return render(request, 'polls/detail.html', context)
+
+
+def result(request, q_id):
+    question = Question.objects.get(id=q_id)
+    choice = Choice.objects.filter(question_id=q_id)
+    voted_choice = request.POST['choice']
+    context = {
+        'question': question,
+        'choice': choice,
+        'voted_choice': voted_choice,
+    }
+    return render(request, 'polls/result.html', context)
+
+
+def vote(request, q_id):
+
+    if request.method == 'POST':
+        question = Question.objects.get(id=q_id)
+        try:
+            voted_choice = request.POST['choice']
+        except MultiValueDictKeyError:
+            choice = Choice.objects.filter(question_id=q_id)
+            context = {
+                'question': question,
+                'choice': choice,
+                'error': "You didn't make a choice!"
+            }
+            return render(request, 'polls/detail.html', context)
+        selected_choice = Choice.objects.get(id=voted_choice)
+        selected_choice.vote += 1
+        selected_choice.save()
+        context = {
+            'question': question,
+            'selected_choice': selected_choice,
+            'voted_choice': voted_choice,
+        }
+        return render(request, 'polls/vote.html', context)
+
+def revote(request, q_id):
+    if request.method == 'POST':
+        voted_choice = request.POST['choice']
+        selected_choice = Choice.objects.get(id=voted_choice)
+        selected_choice.vote -= 1
+        selected_choice.save()
+    return redirect(detail, q_id)
+
